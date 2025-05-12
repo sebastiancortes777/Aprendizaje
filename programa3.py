d6e4054ba9c1bb744e4c0ad226016b38fcfca9aa
program10 <sebastiancortes@MacBook-Air-de-Sebastian.local>
2025-05-11 21:27:56 -0500

    Boolean value

---
+++ programa3.py
@@ -0,0 +1,14 @@
+is_male = False
+is_tall = False
+
+
+if is_male and is_tall:
+    print("you are a tall male")
+elif is_male and not (is_tall):
+    print("you are a short male")
+
+elif not(is_male) and (is_tall):
+    print("you are not a male but you are tall")
+
+else:
+    print ("you are neither not male and not tall")
