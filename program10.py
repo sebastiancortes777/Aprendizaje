9a34563bf28cfbdc49340452c2bdf47305a69949
program10 <sebastiancortes@MacBook-Air-de-Sebastian.local>
2025-05-11 22:10:42 -0500

    calculator

--- program10.py
+++ program10.py
@@ -1,4 +1,19 @@
-friends = ["karina" , "Kevin" , "Luisa"]
-friends [1] = "Mike"
+num1 = float(input ("Enter first number: "))
+op = input ("Enter operator: ")
+num2 = float(input ("Enter second number: "))
 
-print (friends[1])
+if op == "+":
+    print (num1 + num2)
+
+elif op == "-":
+    print (num1 - num2)
+
+elif op == "/":
+    print(num1/num2)
+
+elif op == "*":
+    print(num1*num2)
+
+
+else:
+    print ("invalid operator")
